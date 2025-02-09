a09e0291e5519a2d7e81ff7899e2b6c4d64ea130
Borzou M Alghol <borzoualghol@CES-LD4V7TG0FF.local.lan>
2025-02-09 17:45:46 +1100

    ui

---
+++ amplify/data/addInvoice.js
@@ -0,0 +1,29 @@
+import { util } from "@aws-appsync/utils";
+
+export function request(ctx) {
+  return {
+    method: "POST",
+    resourcePath: "/account.move",
+    params: {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: {
+        name: ctx.arguments.name,
+        move_type: ctx.arguments.move_type,
+        invoice_date: ctx.arguments.invoice_date,
+      },
+    },
+  };
+}
+
+export function response(ctx) {
+  if (ctx.error) {
+    return util.error(ctx.error.message, ctx.error.type);
+  }
+  if (ctx.result.statusCode == 200) {
+    return JSON.parse(ctx.result.body).data;
+  } else {
+    return util.appendError(ctx.result.body, "ctx.result.statusCode");
+  }
+}
