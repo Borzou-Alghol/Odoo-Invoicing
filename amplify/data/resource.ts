b990397c8d2a1ad1c71e7f0d1d2e5a68843c811f
Borzou M Alghol <borzoualghol@CES-LD4V7TG0FF.local.lan>
2025-02-09 17:52:57 +1100

    ui

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,6 +7,12 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
+  Todo: a
+    .model({
+      content: a.string(),
+    })
+    .authorization((allow : any) => [allow.publicApiKey()]),
+
     Invoice: a.customType({
       name: a.string(),
       move_type: a.string(),
