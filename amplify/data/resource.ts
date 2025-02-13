03f08d91c1d9585af1048f6e992dc6fd7a378d6d
Borzou M Alghol <borzoualghol@CES-LD4V7TG0FF.local>
2025-02-13 11:27:36 +1100

    product model

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -13,6 +13,55 @@ const schema = a.schema({
     })
     .authorization((allow : any) => [allow.publicApiKey()]),
 
+    Product: a
+    .model({
+      name: a.string(),
+      EAN: a.string().required(),
+      NationalRefundLogo: a.string(),
+      SaRefundLogo: a.string(),
+      GlassColour: a.string(),
+      PetClour: a.string(),
+      DesignatedCapacityMl: a.string(),
+      HeightMM: a.string(),
+      DiameterMM: a.string(),
+      WeightGM: a.string(),
+      ProductGroupOther: a.string(),
+      GlassColourOther: a.string(),
+      //CreatedBy: a.string(),
+      //CreatedOn: a.string(),
+      //LastModifiedBy: a.string(),
+      //LastModifiedOn: a.string(),
+      MultiSchemeId: a.string(),
+      SupplierParticipantId: a.string(),
+      EffectiveFrom: a.string(),
+      EffectiveTo: a.string(),
+    })
+    .authorization((allow : any) => [allow.publicApiKey()]),
+
+
+//     ID                      NOT NULL VARCHAR2(50)  
+// PRODUCT_GROUP_ID        NOT NULL VARCHAR2(50)  
+// NAME                    NOT NULL VARCHAR2(255) 
+// EAN                              VARCHAR2(20)  
+// NATIONAL_REFUND_LOGO    NOT NULL NUMBER(1)     
+// SA_REFUND_LOGO                   NUMBER(1)     
+// GLASS_COLOUR                     VARCHAR2(255) 
+// PET_COLOUR                       VARCHAR2(255) 
+// DESIGNATED_CAPACITY_ML  NOT NULL FLOAT(126)    
+// HEIGHT_MM               NOT NULL FLOAT(126)    
+// DIAMETER_MM             NOT NULL FLOAT(126)    
+// WEIGHT_GM               NOT NULL FLOAT(126)    
+// PRODUCT_GROUP_OTHER              VARCHAR2(80)  
+// GLASS_COLOUR_OTHER               VARCHAR2(255) 
+// CREATED_BY                       VARCHAR2(255) 
+// CREATED_ON                       TIMESTAMP(6)  
+// LAST_MODIFIED_BY        NOT NULL VARCHAR2(255) 
+// LAST_MODIFIED_ON        NOT NULL DATE          
+// MULTI_SCHEME_ID                  NUMBER        
+// SUPPLIER_PARTICIPANT_ID          NUMBER        
+// EFFECTIVE_FROM                   DATE          
+// EFFECTIVE_TO                     DATE         
+
     Invoice: a.customType({
       name: a.string(),
       move_type: a.string(),
@@ -25,6 +74,7 @@ const schema = a.schema({
     name: a.string(),
     move_type: a.string(),
     invoice_date: a.string().required(),
+
   })
   .returns(a.ref("Invoice"))
   .authorization(allow => [allow.publicApiKey()])
