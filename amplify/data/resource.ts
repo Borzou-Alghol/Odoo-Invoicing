7be78184ee0de96435c44fc4f806238b2b9207b0
Borzou M Alghol <borzoualghol@CES-LD4V7TG0FF.local.lan>
2025-02-14 14:07:28 +1100

    product reg data model

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,83 +7,62 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow : any) => [allow.publicApiKey()]),
 
-    Product: a
+  Product: a
     .model({
       name: a.string(),
+      productId: a.id(),
       EAN: a.string().required(),
-      NationalRefundLogo: a.string(),
-      SaRefundLogo: a.string(),
-      GlassColour: a.string(),
-      PetClour: a.string(),
-      DesignatedCapacityMl: a.string(),
-      HeightMM: a.string(),
-      DiameterMM: a.string(),
-      WeightGM: a.string(),
-      ProductGroupOther: a.string(),
-      GlassColourOther: a.string(),
+      nationalRefundLogo: a.string(),
+      saRefundLogo: a.string(),
+      glassColour: a.string(),
+      petClour: a.string(),
+      designatedCapacityMl: a.string(),
+      heightMM: a.string(),
+      diameterMM: a.string(),
+      weightGM: a.string(),
+      productGroupOther: a.string(),
+      glassColourOther: a.string(),
       //CreatedBy: a.string(),
       //CreatedOn: a.string(),
       //LastModifiedBy: a.string(),
       //LastModifiedOn: a.string(),
-      MultiSchemeId: a.string(),
-      SupplierParticipantId: a.string(),
-      EffectiveFrom: a.string(),
-      EffectiveTo: a.string(),
+      multiSchemeId: a.string(),
+      supplierParticipantId: a.string(),
+      effectiveFrom: a.string(),
+      effectiveTo: a.string(),
+      comments: a.hasMany('Comment', 'productId')
     })
-    .authorization((allow : any) => [allow.publicApiKey()]),
+    .authorization((allow: any) => [allow.publicApiKey()]),
 
+  Comment: a.model({
+    author: a.string(),
+    content: a.string(),
+    product: a.belongsTo('Product', 'productId'),
 
-//     ID                      NOT NULL VARCHAR2(50)  
-// PRODUCT_GROUP_ID        NOT NULL VARCHAR2(50)  
-// NAME                    NOT NULL VARCHAR2(255) 
-// EAN                              VARCHAR2(20)  
-// NATIONAL_REFUND_LOGO    NOT NULL NUMBER(1)     
-// SA_REFUND_LOGO                   NUMBER(1)     
-// GLASS_COLOUR                     VARCHAR2(255) 
-// PET_COLOUR                       VARCHAR2(255) 
-// DESIGNATED_CAPACITY_ML  NOT NULL FLOAT(126)    
-// HEIGHT_MM               NOT NULL FLOAT(126)    
-// DIAMETER_MM             NOT NULL FLOAT(126)    
-// WEIGHT_GM               NOT NULL FLOAT(126)    
-// PRODUCT_GROUP_OTHER              VARCHAR2(80)  
-// GLASS_COLOUR_OTHER               VARCHAR2(255) 
-// CREATED_BY                       VARCHAR2(255) 
-// CREATED_ON                       TIMESTAMP(6)  
-// LAST_MODIFIED_BY        NOT NULL VARCHAR2(255) 
-// LAST_MODIFIED_ON        NOT NULL DATE          
-// MULTI_SCHEME_ID                  NUMBER        
-// SUPPLIER_PARTICIPANT_ID          NUMBER        
-// EFFECTIVE_FROM                   DATE          
-// EFFECTIVE_TO                     DATE         
+  })
+    .authorization((allow: any) => [allow.publicApiKey()]),
 
-    Invoice: a.customType({
+  Invoice: a
+    .customType({
+    }),
+
+  addInvoice: a
+    .mutation()
+    .arguments({
       name: a.string(),
       move_type: a.string(),
       invoice_date: a.string().required(),
-    }),
-      
-  addInvoice: a
-  .mutation()
-  .arguments({
-    name: a.string(),
-    move_type: a.string(),
-    invoice_date: a.string().required(),
 
-  })
-  .returns(a.ref("Invoice"))
-  .authorization(allow => [allow.publicApiKey()])
-  .handler(
-    a.handler.custom({
-      dataSource: "HttpDataSource",
-      entry: "./addInvoice.js",
     })
-  ),
+    .returns(a.ref("Invoice"))
+    .authorization(allow => [allow.publicApiKey()])
+    .handler(
+      a.handler.custom({
+        dataSource: "HttpDataSource",
+        entry: "./addInvoice.js",
+      })
+    ),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -104,7 +83,7 @@ Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
